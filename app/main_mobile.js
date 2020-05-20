1880feae664b8d9962097d0d14cbb77c436e06e3
Druzhinin Aleksandr <sdruzhinin1992@gmail.com>
2020-05-21 01:05:37 +0300

    add opencv constants init in main_mobile.js

--- app/main_mobile.js
+++ app/main_mobile.js
@@ -1,7 +1,17 @@
+
 var logText = document.getElementById("textID");
 var video = document.getElementById("webcam")
 
 var constraints = { video: { facingMode: "environment" }, audio: false };
+
+//opencv constants
+var orb = null;
+var keypoints = null;
+var img_mat_output_with_kp = null;
+var mask = null;
+var desriptors = null;
+var color = null;
+var cap = null;
 
 function cameraStart() {
 	navigator.mediaDevices
@@ -15,7 +25,20 @@ function cameraStart() {
     });
 }
 
+function init_opencv(){
+	orb = new cv.ORB(500);
+	keypoints = new cv.KeyPointVector();
+	img_mat_output_with_kp = new cv.Mat()
+	mask = new cv.Mat()
+	desriptors = new cv.Mat();
+	color = new cv.Scalar(255, 255, 255, 255);
+	cap = new cv.VideoCapture(video);
+}
+
 var start = function () {
+	if (orb === null ){
+		init_opencv()
+	}
 	cameraStart()
 }
 
