77d37fa91622a5d1bc1f9496b76be8193fb704cd
Druzhinin Aleksandr <sdruzhinin1992@gmail.com>
2020-05-21 01:15:53 +0300

    add main flow in main_mobile.js

--- app/main_mobile.js
+++ app/main_mobile.js
@@ -12,6 +12,13 @@ var mask = null;
 var desriptors = null;
 var color = null;
 var cap = null;
+var prev_kp = null;
+var prev_dec = null;
+
+//main flow constants
+var cur_point = null;
+let FPS = 30
+var isFollow = false
 
 //loading opencv.js
 var scr  = document.createElement('script'),
@@ -32,6 +39,7 @@ function cameraStart() {
 		.then(function(stream) {
 		video.srcObject = stream;
 		video.play();
+		processVideo()
     })
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
@@ -48,6 +56,57 @@ function init_opencv(){
 	cap = new cv.VideoCapture(video);
 }
 
+function processVideo() {
+
+	let begin = Date.now();
+	var src = new cv.Mat(640, 480, cv.CV_8UC4);
+
+	cap.read(src);
+
+	cur_keypoints = new cv.KeyPointVector();
+	cur_desriptors = new cv.Mat();
+	orb.detectAndCompute(src, mask, cur_keypoints, cur_desriptors);
+
+	if ( prev_kp == null ){
+		if (cur_desriptors.rows > 0){
+			prev_kp = cur_keypoints
+			prev_dec = cur_desriptors
+		}
+	} else {
+		let cur_matches = match_descriptors(prev_dec, cur_desriptors)
+		let cur_corresp_points = get_corresp_points(cur_matches, prev_kp, cur_keypoints)
+		let array_from = cur_corresp_points["from"]
+		let array_to = cur_corresp_points["to"]
+		let cur_H = getHomography(array_from, array_to)
+
+		if ( (cur_keypoints.size() > 150) && (isFollow) ){
+			if ( cur_point == null ){
+				cur_point = cv.matFromArray(1, 1, cv.CV_32FC2, [240, 320]);
+			} else {
+				cur_point = point_perspective_transform(cur_point, cur_H)
+			}
+			prev_kp = cur_keypoints
+			prev_dec = cur_desriptors
+		}
+	}
+	
+	cv.drawKeypoints(src, cur_keypoints, img_mat_output_with_kp);
+
+	if ( cur_point == null ){
+		let center = new cv.Point(240, 320);
+		cv.circle(src, center, 5, color, -1);
+	} else {
+		let center = new cv.Point(cur_point.data32F[0], cur_point.data32F[1]);
+		cv.circle(src, center, 5, color, -1);
+	}
+	
+
+	let script_time = Date.now() - begin
+	cv.imshow("image_canvas", src);
+	let delay =Math.max(1000/FPS - (script_time), 0);
+	setTimeout(processVideo, delay);	
+}
+
 var start = function () {
 	if (orb === null ){
 		init_opencv()
@@ -56,5 +115,5 @@ var start = function () {
 }
 
 var place = function () {
-
+	isFollow = true	
 }
